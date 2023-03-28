4236ad4045be116a5588d7ce676c767f674d37b4
tungtran0911 <tranthanhtung020520059112005@gmail.com>
2023-03-28 20:13:33 +0700

    update

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SafeAreaView style={{ flex: 1 }}>
+        <RootStack />
+      </SafeAreaView>
+      <StatusBar backgroundColor="black" />
     </View>
   );
 }
